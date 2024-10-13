3b7891d16deddc7eed25481bcc3ccc08dd2788b7
vik-sos <v.ubedaramirez@alpha-nex.com>
2024-10-13 17:23:58 -0300

    Se realizan en instrucciones deprecadas y se factoriza en regiones para mayor comprensión de código

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const { app, BrowserWindow, BrowserView, ipcMain } = require('electron');
+const {
+  app,
+  BaseWindow,
+  WebContentsView,
+  ipcMain,
+  Menu,
+  MenuItem,
+} = require('electron');
 const path = require('path');
 
 let mainWindow;
 let googleView;
 let sidebarView;
+let frameView;
 
+//#region Crear la ventana principal
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+  mainWindow = new BaseWindow({
+    width: 1280,
+    frame: true,
+    minHeight: 800,
+    minWidth: 1200,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'src', 'preload.js'),
       contextIsolation: true,
@@ -18,78 +30,156 @@ function createWindow() {
     },
   });
 
-  // Crear la vista de la sidebar
-  sidebarView = new BrowserView({
+  //#region Crear la vista del frame
+  frameView = new WebContentsView({
+    webPreferences: {
+      height: 30,
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  mainWindow.contentView.addChildView(frameView);
+  frameView.webContents.loadFile(
+    path.join(__dirname, 'src', 'pages', 'frame.html')
+  );
+  //#endregion
+
+  //#region Crear la vista de la sidebar
+  sidebarView = new WebContentsView({
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
-  mainWindow.setBrowserView(sidebarView);
+  mainWindow.contentView.addChildView(sidebarView);
   sidebarView.webContents.loadFile(
     path.join(__dirname, 'src', 'pages', 'sidebar.html')
   );
+  //#endregion
 
-  // Crear vista para cuadro de busqueda
-
-  searchView = new BrowserView({
+  //#region Crear vista para cuadro de busqueda
+  searchView = new WebContentsView({
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
-  mainWindow.addBrowserView(searchView);
+  mainWindow.contentView.addChildView(searchView);
   searchView.webContents.loadFile(
     path.join(__dirname, 'src', 'pages', 'search.html')
   );
+  //#endregion
 
-  // Crear la vista para Google
-  googleView = new BrowserView({
+  //#region Crear la vista para Google
+  googleView = new WebContentsView({
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
-  mainWindow.addBrowserView(googleView);
+  mainWindow.contentView.addChildView(googleView);
   googleView.webContents.loadURL('https://www.google.com');
+  //#endregion
 
-  // Ajustar las vistas al tamaño de la ventana
+  //#region Ajustar las vistas al tamaño de la ventana
   function adjustViews() {
     const { width, height } = mainWindow.getBounds();
-    const sidebarWidth = 75;
+    const sidebarWidth = 150;
+    const frameHeight = 0;
     sidebarView.setBounds({ x: 0, y: 0, width: sidebarWidth, height: height });
     googleView.setBounds({
       x: sidebarWidth,
-      y: 0,
+      y: frameHeight,
       width: width - sidebarWidth,
-      height: height,
+      height: height - 30,
+    });
+    frameView.setBounds({
+      x: 0,
+      y: 0,
+      width: width,
+      height: 30,
     });
   }
 
   mainWindow.on('resize', adjustViews);
   adjustViews();
+  //#endregion
 
-  // Escuchar los eventos de carga de Google y notificar a la sidebar
+  //Escuchar los eventos de carga de Google y notificar a la sidebar
   googleView.webContents.on('did-start-loading', () => {
     sidebarView.webContents.send('loading-started');
   });
 
+  //Notificar termino de carga y realizar scrape
   googleView.webContents.on('did-stop-loading', () => {
     sidebarView.webContents.send('loading-stopped');
+    scrapeCurrent();
   });
 
-  // Manejar eventos IPC desde la sidebar para navegar a diferentes URLs
+  //#region Manejar eventos IPC desde la sidebar para navegar a diferentes URLs
   ipcMain.on('navigate-to', (event, url) => {
     if (googleView && googleView.webContents) {
       googleView.webContents.loadURL(url);
     }
   });
 
+  //#endregion
+
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
+
+  //#region Menú de la aplicación
+  const menu = Menu();
+  menu.append(
+    new MenuItem({
+      label: 'Electron',
+      submenu: [
+        {
+          label: 'Exit',
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    })
+  );
+  menu.append(
+    new MenuItem({
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          click: () => {
+            mainWindow.reload();
+          },
+        },
+        {
+          label: 'Toggle DevTools',
+          click: () => {
+            mainWindow.toggleDevTools();
+          },
+        },
+      ],
+    })
+  );
+  Menu.setApplicationMenu(menu);
+  //#endregion
 }
 
+//#endregion
+
+//#region Funciones de teclado
+
+//#region Funciones de scraping
+async function scrapeCurrent() {
+  const html = await googleView.webContents.executeJavaScript(
+    'document.body.innerHTML'
+  ); // Obtener el HTML de la página actual
+}
+//#endregion
+
+//#region Manejo de eventos de la app
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
@@ -103,3 +193,4 @@ app.on('activate', () => {
     createWindow();
   }
 });
+//#endregion
