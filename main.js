650d8e6b5e253187138e5b7573e90f9acfa0e55f
Victor U. Ramirez <benjamin@alphanex.cl>
2024-10-17 19:23:21 -0300

    Se agregan ajustes para navegación en google por teclado

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const { app, BaseWindow, WebContentsView, ipcMain, Menu, MenuItem, globalShortcut } = require("electron");
-const path = require("path");
-const OpenAI = require("openai");
-require("dotenv").config();
+const {
+  app,
+  BaseWindow,
+  WebContentsView,
+  ipcMain,
+  Menu,
+  MenuItem,
+  globalShortcut,
+} = require('electron');
+const path = require('path');
+const OpenAI = require('openai');
+require('dotenv').config();
 
 // Inicializar OpenAI con la clave de API desde las variables de entorno
 const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY, // Asegúrate de que la clave esté en tus variables de entorno
+  apiKey: process.env.OPENAI_API_KEY, // Asegúrate de que la clave esté en tus variables de entorno
 });
 
 let mainWindow;
@@ -13,8 +21,11 @@ let googleView;
 let sidebarView;
 let frameView;
 
-//#region Crear la ventana principal
+let keyboardNavigation = false;
+
+//#region Crear la ventana app
 function createWindow() {
+  //#region Crear ventana principal
   mainWindow = new BaseWindow({
     width: 1280,
     frame: true,
@@ -23,12 +34,13 @@ function createWindow() {
     height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'src', 'preload.js'),
-      contextIsolation: true,
+      contextIsolation: false,
       enableRemoteModule: false,
-      nodeIntegration: false,
+      nodeIntegration: true,
       webSecurity: true,
     },
   });
+  //#endregion
 
   //#region Crear la vista del frame
   frameView = new WebContentsView({
@@ -39,6 +51,7 @@ function createWindow() {
     },
   });
   mainWindow.contentView.addChildView(frameView);
+
   frameView.webContents.loadFile(
     path.join(__dirname, 'src', 'pages', 'frame.html')
   );
@@ -73,8 +86,8 @@ function createWindow() {
   //#region Crear la vista para Google
   googleView = new WebContentsView({
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   });
   mainWindow.contentView.addChildView(googleView);
@@ -105,35 +118,35 @@ function createWindow() {
   adjustViews();
   //#endregion
 
-	//#region Manejar eventos IPC desde la sidebar para realizar la consulta a OpenAI
-	ipcMain.on("search-query", async (event, query) => {
-		try {
-			// Hacer la solicitud a OpenAI utilizando tu GPT personalizado
-			const response = await openai.chat.completions.create({
-				model: "gpt-4o",
-				messages: [
-					{
-						role: "system",
-						content:
-							"eres un navegador, tienes que buscar dentro de las primeras 6 opciones de google cuando alguien te pregunte algo, mostrando en primer lugar el link que mas se asemeje a lo que te preguntan, también haces un resumen de lo mas importante que se está preguntando y muestras posibles links que estén relacionado a la información que sacaste para hacer el resumen. El resumen debes dejarlo bien legible y que se entienda bien para todo tipo de personas",
-					},
-					{
-						role: "user",
-						content: query,
-					},
-				],
-				temperature: 1,
-				max_tokens: 2048,
-				top_p: 1,
-				frequency_penalty: 0,
-				presence_penalty: 0,
-			});
+  //#region Manejar eventos para realizar la consulta a OpenAI
+  ipcMain.on('search-query', async (event, query) => {
+    try {
+      // Hacer la solicitud a OpenAI utilizando tu GPT personalizado
+      const response = await openai.chat.completions.create({
+        model: 'gpt-4o',
+        messages: [
+          {
+            role: 'system',
+            content:
+              'eres un navegador, tienes que buscar dentro de las primeras 6 opciones de google cuando alguien te pregunte algo, mostrando en primer lugar el link que mas se asemeje a lo que te preguntan, también haces un resumen de lo mas importante que se está preguntando y muestras posibles links que estén relacionado a la información que sacaste para hacer el resumen. El resumen debes dejarlo bien legible y que se entienda bien para todo tipo de personas',
+          },
+          {
+            role: 'user',
+            content: query,
+          },
+        ],
+        temperature: 1,
+        max_tokens: 2048,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
 
-			const result = response.choices[0].message.content.trim();
+      const result = response.choices[0].message.content.trim();
 
-			// Mostrar los resultados en la vista de Google
-			googleView.webContents.loadURL(
-				`data:text/html;charset=utf-8,${encodeURIComponent(`
+      // Mostrar los resultados en la vista de Google
+      googleView.webContents.loadURL(
+        `data:text/html;charset=utf-8,${encodeURIComponent(`
                 <!DOCTYPE html>
                 <html>
                 <head>
@@ -176,14 +189,14 @@ function createWindow() {
                 </body>
                 </html>
             `)}`
-			);
+      );
 
-			sidebarView.webContents.send("loading-stopped"); // Detener indicador de carga en la sidebar
-		} catch (error) {
-			console.error("Error al hacer la búsqueda:", error);
-			sidebarView.webContents.send("loading-stopped");
-			googleView.webContents.loadURL(
-				`data:text/html;charset=utf-8,${encodeURIComponent(`
+      sidebarView.webContents.send('loading-stopped'); // Detener indicador de carga en la sidebar
+    } catch (error) {
+      console.error('Error al hacer la búsqueda:', error);
+      sidebarView.webContents.send('loading-stopped');
+      googleView.webContents.loadURL(
+        `data:text/html;charset=utf-8,${encodeURIComponent(`
                 <!DOCTYPE html>
                 <html>
                 <head>
@@ -207,9 +220,9 @@ function createWindow() {
                 </body>
                 </html>
             `)}`
-			);
-		}
-	});
+      );
+    }
+  });
 
   //#endregion
 
@@ -224,7 +237,7 @@ function createWindow() {
     scrapeCurrent();
   });
 
-	//#region Manejar eventos IPC desde la sidebar para navegar a diferentes URLs
+  //#region Manejar eventos IPC desde la sidebar para navegar a diferentes URLs
   ipcMain.on('navigate-to', (event, url) => {
     if (googleView && googleView.webContents) {
       googleView.webContents.loadURL(url);
@@ -233,9 +246,9 @@ function createWindow() {
 
   //#endregion
 
-	mainWindow.on("closed", () => {
-		mainWindow = null;
-	});
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 
   //#region Menú de la aplicación
   const menu = Menu();
@@ -273,25 +286,120 @@ function createWindow() {
   );
   Menu.setApplicationMenu(menu);
   //#endregion
-}
 
-//#endregion
+  //#region Swipe gestures
+  googleView.webContents.on('did-finish-load', () => {
+    googleView.webContents.executeJavaScript(`
+      const { ipcRenderer } = require('electron');
+      let lastDeltaX = 0;
+      window.addEventListener('wheel', (event) => {
+        // Detectar gestos horizontales significativos
+        if (Math.abs(event.deltaX) > Math.abs(event.deltaY) && Math.abs(event.deltaX - lastDeltaX) > 50) {
+          if (event.deltaX > 0) {
+            ipcRenderer.send('wheel-swipe', 'right');
+          } else {
+            ipcRenderer.send('wheel-swipe', 'left');
+          }
+          lastDeltaX = event.deltaX;
+        }
+      });
+      document.addEventListener('keydown', (event) => {
+        // Detectar si el foco está en un campo de texto o textarea
+        const activeElement = document.activeElement;
+        const isInput = activeElement.tagName === 'INPUT' || activeElement.tagName === 'TEXTAREA' || activeElement.isContentEditable;
 
-//#region Funciones de teclado
-function registerShortcuts() {
-  globalShortcut.register('CommandOrControl+R', () => {
-    mainWindow.reload();
+        if (!isInput) {
+          // Si no está en un input, las flechas harán scroll
+          if (event.key === 'ArrowUp') {
+            window.scrollBy(0, -100); // Scroll hacia arriba
+            event.preventDefault(); // Evitar que el evento se propague
+          }
+          if (event.key === 'ArrowDown') {
+            window.scrollBy(0, 100); // Scroll hacia abajo
+            event.preventDefault(); // Evitar que el evento se propague
+          }
+        }
+      });
+    `);
   });
 
-  globalShortcut.register('CommandOrControl+Shift+I', () => {
-    mainWindow.toggleDevTools();
+  ipcMain.on('wheel-swipe', (event, direction) => {
+    if (direction === 'right') {
+      if (googleView.webContents.navigationHistory.canGoForward()) {
+        googleView.webContents.navigationHistory.goForward();
+      }
+    } else if (direction === 'left') {
+      if (googleView.webContents.navigationHistory.canGoBack()) {
+        googleView.webContents.navigationHistory.goBack();
+      }
+    }
   });
+  //#endregion
 }
+
+async function registerShortcuts() {
+  try {
+    await googleView.webContents.executeJavaScript(`
+    let currentElementIndex = 0;
+    let interactiveElements = Array.from(document.querySelectorAll('a, button, [role="button"], [role="link"], div[onclick], span[onclick]'));
+
+    if (interactiveElements.length > 0) {
+      // Resaltar el primer elemento interactivo
+      interactiveElements[currentElementIndex].focus();
+    }
+
+    document.addEventListener('keydown', handleNavigation);
+
+    function handleNavigation(event) {
+      if (!['ArrowUp', 'ArrowDown', 'ArrowRight'].includes(event.key)) return;
+
+      if (event.key === 'ArrowDown') {
+        // Moverse al siguiente elemento interactivo
+        currentElementIndex = (currentElementIndex + 1) % interactiveElements.length;
+        interactiveElements[currentElementIndex].focus();
+        event.preventDefault();
+      }
+
+      if (event.key === 'ArrowUp') {
+        // Moverse al elemento interactivo anterior
+        currentElementIndex = (currentElementIndex - 1 + interactiveElements.length) % interactiveElements.length;
+        interactiveElements[currentElementIndex].focus();
+        event.preventDefault();
+      }
+
+      if (event.key === 'ArrowRight') {
+        // Simular un clic en el elemento seleccionado
+        const currentElement = interactiveElements[currentElementIndex];
+        if (typeof currentElement.click === 'function') {
+          currentElement.click();
+        } else {
+          // Intentar simular clic en caso de que no tenga un método 'click'
+          let event = new MouseEvent('click', { bubbles: true, cancelable: true });
+          currentElement.dispatchEvent(event);
+        }
+        event.preventDefault();
+      }
+
+      if (event.key === 'ArrowLeft') {
+      }
+    }
+    `);
+  } catch (error) {
+    console.error('Error al registrar atajos de teclado:', error);
+  }
+}
+
+async function deactivateShortcuts() {
+  await googleView.webContents.executeJavaScript(`
+    document.removeEventListener('keydown', (event) => {}); // Remover el listener de keydown
+  `);
+}
+
 //#endregion
 
 //#region Funciones de scraping
-async function scrapeCurrent() {
-  const html = await googleView.webContents.executeJavaScript(
+function scrapeCurrent() {
+  const html = googleView.webContents.executeJavaScript(
     'document.body.innerHTML'
   ); // Obtener el HTML de la página actual
 }
@@ -300,9 +408,19 @@ async function scrapeCurrent() {
 //#region Manejo de eventos de la app
 app.whenReady().then(() => {
   createWindow();
-  globalShortcut.register('B+N+M', () => {
-    registerShortcuts();
+
+  globalShortcut.register('Shift+Z+X', () => {
+    keyboardNavigation = !keyboardNavigation;
+    if (keyboardNavigation) {
+      registerShortcuts();
+    } else {
+      deactivateShortcuts();
+    }
   });
+
+  //#region Funciones de teclado
+
+  //#endregion
 
   //#region Handling shortcuts
   globalShortcut.register('CommandOrControl+W', () => {
@@ -311,19 +429,23 @@ app.whenReady().then(() => {
   globalShortcut.register('CommandOrControl+Q', () => {
     app.quit();
   });
+  globalShortcut.register('CommandOrControl+M', () => {
+    mainWindow.minimize();
+  });
+
   //#endregion
 });
 
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on("activate", () => {
-	if (mainWindow === null) {
-		createWindow();
-	}
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 app.on('will-quit', () => {
